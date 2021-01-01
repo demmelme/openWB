980bb29d754977ead132ed934f331cdb92ca1af4
demmelme <christian_demmelmeier@gmx.de>
2021-01-01 13:25:58 +0100

    Update soc.py
    create fork

--- modules/soc_leaf/soc.py
+++ modules/soc_leaf/soc.py
@@ -2,7 +2,6 @@
 import sys
 import time
 
-
 leaftimer = open('/var/www/html/openWB/ramdisk/soctimer', 'r')
 leaftimer = int(leaftimer.read())
 thecommand=str('python3 /var/www/html/openWB/modules/soc_leaf/getsoc.py')
